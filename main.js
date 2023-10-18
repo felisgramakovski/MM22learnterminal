21fe487a718536f855a274d317ff7c281236a22c
Felis Gramakovski <felis.gramakovski@gmail.com>
2023-10-18 14:19:46 +0300

    kastid

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function write(text) {
     process.stdout.write(text);
 }
 
-console.log(chalk.bgGreen.magenta('Hello world'));
+console.log(chalk.bgGreen.magenta('Hello world'), {padding: 5, margin: 300, borderStyle:'arrow', borderColor: 'redBright'});
 
-console.log(boxen('tsauki', {padding: 1}));
+console.log(boxen(chalk.magenta('tsauks'), {padding: 1, margin: 1, borderStyle: 'classic'}));
 
-console.log(boxen('mis teoksil', {padding: 1, margin: 1, borderStyle: 'double'}));
+console.log(boxen(chalk.cyan('pasta'), {padding: 5, margin: 2, borderStyle:'arrow', borderColor: 'yellowBright'}));
 
-console.log(boxen('tahaks süüa', {title: 'magical', titleAlignment: 'center'}));
+console.log(boxen(chalk.bgCyan.strikethrough('jouuu'), {padding: 5, margin: 1, borderStyle:'bold'}));
 
-console.log(boxen('varsti saab koju', {title: 'example'}));
+console.log(boxen(chalk.bgRed.green('söök'), {padding: 10, margin: 1, title: 'nämma', titleAlignment: 'center', borderColor: 'cyanBright'}));
 
-console.log(boxen('vihm', {width: 15}));
+console.log(boxen(chalk.bgGreen.inverse('hahahaha'), {padding: 1, margin: 1, borderStyle:'classic'}));
